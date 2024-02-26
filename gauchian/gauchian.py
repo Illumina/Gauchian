92e143a6b209cb7fc0f1f2f740ad72058ccf9b6c
Guillaume Rizk <grizk@illumina.com>
2024-02-26 15:21:26 +0100

    update license header

--- gauchian/gauchian.py
+++ gauchian/gauchian.py
@@ -6,18 +6,15 @@
 #
 # Author: Xiao Chen <xchen2@illumina.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# at your option) any later version.
+# This program is licensed under the terms of the Polyform strict license
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# ***As far as the law allows, the software comes as is, without
+# any warranty or condition, and the licensor will not be liable
+# to you for any damages arising out of these terms or the use
+# or nature of the software, under any kind of legal claim.***
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the PolyForm Strict License 1.0.0
+# along with this program.  If not, see <https://polyformproject.org/licenses/strict/1.0.0>.
 #
 #
 
